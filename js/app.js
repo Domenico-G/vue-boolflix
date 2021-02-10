d6d5bb7a1ef47989f3c8de9218edbf703b952d9f
Domenico <domenicograsso@gmail.com>
2021-02-10 14:49:31 +0100

    fine milestone 2

--- js/app.js
+++ js/app.js
@@ -8,13 +8,31 @@ new Vue({
     getMovie: function () {
       const self = this;
       axios
-        .get(
-          "https://api.themoviedb.org/3/search/movie?api_key=a125714ea457f065269757ad3c9d6db9&query=" +
-            this.titleMovie
-        )
-        .then(function (resp) {
-          self.listMovie = resp.data.results;
-        });
+        .all([
+          axios.get(
+            "https://api.themoviedb.org/3/search/movie?api_key=a125714ea457f065269757ad3c9d6db9&query=" +
+              this.titleMovie
+          ),
+          axios.get(
+            "https://api.themoviedb.org/3/search/tv?api_key=a125714ea457f065269757ad3c9d6db9&query=" +
+              this.titleMovie
+          ),
+        ])
+        .then(
+          axios.spread(function (respM, respT) {
+            console.log(respT.data.results[0].title);
+            //creo una lista di film e serieTV tramite query
+            self.listMovie = respM.data.results.concat(respT.data.results);
+            self.listMovie.forEach((item) => {
+              //cambio il voto dei film da decimale in intero
+              item.vote_average = Math.ceil(item.vote_average / 2);
+            });
+          })
+        );
+    },
+    // funzione per stelle vuote
+    emptyStars: function (stars) {
+      return 5 - stars;
     },
   },
 });
